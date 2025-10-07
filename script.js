c6a185c22d8346cf0cf7610a31d2bc00f0ff5d78
azkafawwazah <azkafawwazah150209@gmail.com>
2025-10-07 12:08:51 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const tombolSapa = document.getElemenByld(`sapaButton`)
-tombolSapa.addEventListener('click', funcation(){
+tombolSapa.addEventListener('click', function(){
 alert('Halo! Terima kasih sudah berkunjung!');
 });
